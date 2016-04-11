d06e5963a6d91603223a74578ac342521f56db87
Joe Wanat <joseph.wanat@zoro.com>
2016-04-11 13:07:47 -0500

    Added function scraping main page

--- bayonetPrices.js
+++ bayonetPrices.js
@@ -1,7 +1,8 @@
 var request = require("request"),
 cheerio = require("cheerio"),
 urlFactoryNew = "https://steamcommunity.com/market/listings/730/%E2%98%85%20Bayonet%20%7C%20Doppler%20%28Factory%20New%29",
-urlMinWear = "https://steamcommunity.com/market/listings/730/%E2%98%85%20Bayonet%20%7C%20Doppler%20%28Minimal%20Wear%29";
+urlMinWear = "https://steamcommunity.com/market/listings/730/%E2%98%85%20Bayonet%20%7C%20Doppler%20%28Minimal%20Wear%29",
+urlDopplerBayo = "https://steamcommunity.com/market/search?category_730_ItemSet%5B%5D=any&category_730_ProPlayer%5B%5D=any&category_730_StickerCapsule%5B%5D=any&category_730_TournamentTeam%5B%5D=any&category_730_Weapon%5B%5D=tag_weapon_bayonet&appid=730&q=doppler";
 
 request(urlFactoryNew, function (error, response, body) {
   if (!error) {
@@ -14,8 +15,23 @@ request(urlFactoryNew, function (error, response, body) {
 
     prices.join(', ');
 
-    console.log(prices);
+    console.log('Factory New Prices: ' + prices);
 
+  } else {
+    console.log("We’ve encountered an error: " + error);
+  }
+});
+
+
+request(urlDopplerBayo, function (error, response, body) {
+  if (!error) {
+    var $ = cheerio.load(body);
+
+    $('.market_listing_item_name').each(function(i, elem) {
+        price  = $(this).parents('div.market_listing_searchresult').find('span.sale_price').text();
+        name = $(this).text().trim();
+        console.log(name + ': ' + price);
+    });
   } else {
     console.log("We’ve encountered an error: " + error);
   }
