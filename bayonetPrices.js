f3a9949144287f5199b36da2d0d9c487200955fe
Joe Wanat <joseph.wanat@zoro.com>
2016-04-11 11:01:32 -0500

    Working but with list of list

---
+++ bayonetPrices.js
@@ -0,0 +1,23 @@
+var request = require("request"),
+cheerio = require("cheerio"),
+urlFactoryNew = "https://steamcommunity.com/market/listings/730/%E2%98%85%20Bayonet%20%7C%20Doppler%20%28Factory%20New%29",
+urlMinWear = "https://steamcommunity.com/market/listings/730/%E2%98%85%20Bayonet%20%7C%20Doppler%20%28Minimal%20Wear%29";
+
+request(urlFactoryNew, function (error, response, body) {
+  if (!error) {
+    var $ = cheerio.load(body);
+    var prices  = [];
+
+    $('.market_listing_price_with_fee').each(function(i, elem) {
+        price = $(this).text().replace(/\t/g, '').split('\r\n');
+        prices[i] = price;
+    });
+
+    prices.join(', ');
+
+    console.log(prices);
+
+  } else {
+    console.log("We’ve encountered an error: " + error);
+  }
+});
