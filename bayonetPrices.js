3eae37ec91fddb28a50b6143f543387b3bf53e8e
Joe Wanat <joseph.wanat@zoro.com>
2016-04-11 14:47:54 -0500

    Added mailgun dependency and updated get request

--- bayonetPrices.js
+++ bayonetPrices.js
@@ -1,39 +1,34 @@
 var request = require("request"),
-cheerio = require("cheerio"),
-nodemailer = require("nodemailer"),
-urlFactoryNew = "https://steamcommunity.com/market/listings/730/%E2%98%85%20Bayonet%20%7C%20Doppler%20%28Factory%20New%29",
-urlMinWear = "https://steamcommunity.com/market/listings/730/%E2%98%85%20Bayonet%20%7C%20Doppler%20%28Minimal%20Wear%29",
-urlDopplerBayo = "https://steamcommunity.com/market/search?category_730_ItemSet%5B%5D=any&category_730_ProPlayer%5B%5D=any&category_730_StickerCapsule%5B%5D=any&category_730_TournamentTeam%5B%5D=any&category_730_Weapon%5B%5D=tag_weapon_bayonet&appid=730&q=doppler";
+    cheerio = require("cheerio"),
+    urlDopplerBayo = "https://steamcommunity.com/market/search?category_730_ItemSet%5B%5D=any&category_730_ProPlayer%5B%5D=any&category_730_StickerCapsule%5B%5D=any&category_730_TournamentTeam%5B%5D=any&category_730_Weapon%5B%5D=tag_weapon_bayonet&appid=730&q=doppler";
 
-request(urlFactoryNew, function (error, response, body) {
-  if (!error) {
-    var $ = cheerio.load(body);
-    var prices  = [];
+var api_key = 'key-63a5ebafb80f1d576c0c0a6d01007ea1';
+var mg_domain = 'sandboxe4257b7ad2204297a4a8e920ec45a455.mailgun.org';
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: mg_domain});
 
-    $('.market_listing_price_with_fee').each(function(i, elem) {
-        prices[i] = $(this).text().trim();
-    });
-
-    prices.join(', ');
-
-    console.log('Factory New Prices: ' + prices);
-
-  } else {
-    console.log("We’ve encountered an error: " + error);
-  }
-});
-
+var knifeList = [];
 
 request(urlDopplerBayo, function (error, response, body) {
   if (!error) {
     var $ = cheerio.load(body);
 
     $('.market_listing_item_name').each(function(i, elem) {
-        price  = $(this).parents('div.market_listing_searchresult').find('span.sale_price').text();
-        name = $(this).text().trim();
-        console.log(name + ': ' + price);
+      price  = $(this).parents('div.market_listing_searchresult').find('span.sale_price').text();
+      name = $(this).text().trim();
+      knifeList.push(name + ': ' + price);
     });
   } else {
     console.log("We’ve encountered an error: " + error);
   }
+});
+
+var data = {
+  from: 'Doug Likes Nipples <me@samples.mailgun.org>',
+  to: 'wanat.joe@gmail.com',
+  subject: 'Hello, fat boy!',
+  text: 'Testing some Mailgun awesomness!'
+};
+
+mailgun.messages().send(data, function (error, body) {
+  console.log(body);
 });
